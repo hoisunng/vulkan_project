c4e82c9b7e932d77602464e70b69c1340520fe22
Hoisun Ng <hoisun.ng@gmail.com>
2020-09-05 23:54:20 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+﻿#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	const auto window = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << "Extension count: " << extensionCount << "\n";
+
+	glm::mat4 testMatrix{ 1.0 };
+	glm::vec4 testVector{ 1.0 };
+	auto testResult = testMatrix * testVector;
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
+}
